15005e99ed2ef9b3e2d32149ef4db420b3a9bc8d
rain84 <rainy_sky@mail.ru>
2021-06-19 12:31:37 +0300

    fix: typo in filename

--- src/features/authorization/authorizationSlice.ts
+++ src/features/authorization/authorizationSlice.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState, AppThunk } from 'store';
-import { fetchAuth } from './ authorizationAPI';
+import { fetchAuth } from './authorizationAPI';
 
 export interface authorizationState {
   isAuthorized: boolean;
